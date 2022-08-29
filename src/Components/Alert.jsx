e3682fc48de3995a325487e9b0aeb0607c7111b0
Pacifist <desmondmorales92@gmail.com>
2022-08-29 13:01:51 +0300

    classNames, props

---
+++ src/Components/Alert.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Alert extends React.Component {
+    render() {
+        return (
+            <div className={"alert " + "alert-" + this.props.type} role="alert">{this.props.text}</div>
+        )
+    }
+}
+
+export default Alert
