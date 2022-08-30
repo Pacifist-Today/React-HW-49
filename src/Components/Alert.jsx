86a12099892f4aae7fd0efcd7653393f494168d3
Pacifist <desmondmorales92@gmail.com>
2022-08-30 12:18:25 +0300

    destructurization, validation

--- src/Components/Alert.jsx
+++ src/Components/Alert.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
+const checkType = (checkType) => {
+    const arrTypes = ["primary", "secondary", "success", "danger", "warning", "info", "light", "dark"]
+    return arrTypes.includes(checkType)
+}
+
 class Alert extends React.Component {
     render() {
+        const {type, text} = this.props
         return (
-            <div className={"alert " + "alert-" + this.props.type} role="alert">{this.props.text}</div>
+            <div className={`alert alert-${checkType(type) ? type : 'danger'}`} role="alert">{checkType(type) ? text : 'This style doesn\'t exist'}</div>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import Alert from "./Components/Alert";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <Alert type="warning" text="what is love?" />
+      <Alert type="ab" text="what is love?" />
   </React.StrictMode>
 );
