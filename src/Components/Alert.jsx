11ccea3114ff2f80931cd129818abea6959c83bd
Pacifist <desmondmorales92@gmail.com>
2022-08-30 12:18:50 +0300

    destructurization, validation

--- src/Components/Alert.jsx
+++ src/Components/Alert.jsx
@@ -9,7 +9,8 @@ class Alert extends React.Component {
     render() {
         const {type, text} = this.props
         return (
-            <div className={`alert alert-${checkType(type) ? type : 'danger'}`} role="alert">{checkType(type) ? text : 'This style doesn\'t exist'}</div>
+            <div className={`alert alert-${checkType(type) ? type : 'danger'}`} role="alert">
+                {checkType(type) ? text : 'This style doesn\'t exist'}</div>
         )
     }
 }
